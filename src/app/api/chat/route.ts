e6fce9ef420d132629333adc5bec0382a92c901c
Phat0101 <47703758+Phat0101@users.noreply.github.com>
2024-10-06 16:14:37 +1100

    fix reading local file in api/chat/route

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -2,13 +2,15 @@ import { streamText } from "ai";
 // import { openai } from '@ai-sdk/openai';
 import { google } from '@ai-sdk/google';
 import { promises as fs } from 'fs';
+import path from 'path';
 
 export async function POST(req: Request, res: Response) {
   const { messages } = await req.json();
   const messageContent = messages[messages.length - 1].content;
 
   // Read the content of the Markdown file
-  const resumeContent = await fs.readFile(process.cwd() + '/public/Patrick_Resume.md', 'utf-8');
+  const resumeDirectory = path.join(process.cwd(), 'public');
+  const resumeContent = await fs.readFile(resumeDirectory + '/Patrick_Resume.md', 'utf-8');
 
   const response = await streamText({
     // model: openai('gpt-4o-mini'),
